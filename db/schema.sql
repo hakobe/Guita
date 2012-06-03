c3cefc195c2955ccea5456ec582fe51b1c96fd6c
hakobe <hakobe@gmail.com>
2012-06-03 19:24:19 +0900

    nameカラム追加などテーブル定義修正

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,24 @@
 CREATE TABLE `pick` (
     `uuid`        BIGINT UNSIGNED NOT NULL,
-    `user_id`     VARCHAR(255) NOT NULL,
+    `user_id`     BIGINT UNSIGNED NOT NULL,
     `description` TEXT NOT NULL DEFAULT '',
     `created`     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     KEY `user` (`user_id`, `created`),
     KEY `created` (`created`),
     PRIMARY KEY (`uuid`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=binary;
 
 CREATE TABLE `user` (
     `uuid`        BIGINT UNSIGNED NOT NULL,
     `github_id`   INTEGER UNSIGNED NOT NULL,
+    `name`        VARCHAR(128) NOT NULL,
     `sk`          VARCHAR(255) NOT NULL,
     `sk_expires`  TIMESTAMP NOT NULL,
     `struct`      TEXT NOT NULL DEFAULT '',
 
     UNIQUE KEY (`github_id`),
     UNIQUE KEY (`sk`),
+    UNIQUE KEY (`name`),
     PRIMARY KEY (`uuid`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=binary;
